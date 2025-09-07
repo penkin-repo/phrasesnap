87dacd54784bd462095550fef968922f212f0763
iampenkin@gmail.com <54068102+penkin-repo@users.noreply.github.com>
2025-09-08 00:15:45 +0300

    fix manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   // output: 'static',
   site: 'https://penkin-repo.github.io/phrasesnap/',
-  // base: '/phrasesnap',
+  base: '/phrasesnap',
   vite: {
     plugins: [tailwindcss()]
   },
